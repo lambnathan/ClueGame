6095153881bf7f611b6e7dbd9459a23423950bcb
Nathan <nlambert@mymail.mines.edu>
2019-02-28 13:59:12 -0700

    Added new test java files and data files

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,13 @@
+//Authors: Nathan Lambert and Elliott McCabe
+package clueGame;
+
+public class BoardCell {
+	private int row;
+	private int column;
+	
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+}
