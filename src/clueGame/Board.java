1ec05524406650971812140758d9c4c2ce7c8e79
Elliott McCabe <mccabe@mines.edu>
2019-02-28 14:30:18 -0700

    added enum DoorDirection, BoardCell class within experiment, updated BoardCell class within clueGame, added BadConfigFormatException, added Board class

---
+++ src/clueGame/Board.java
@@ -0,0 +1,9 @@
+package clueGame;
+
+public class Board {
+	public static final int MAX_BOARD_SIZE = 50;
+	private static int numRows = 26;
+	private static int numColumns = 26;
+	private BoardCell[][] board;
+	private HashMap<char Character> legend;
+}
---
+++ src/clueGame/DoorDirection.java
@@ -0,0 +1,10 @@
+package clueGame;
+
+public enum DoorDirection {
+	UP("U"), DOWN("D"), LEFT("L"), RIGHT("R"), NONE("N");
+	private String temp;
+	
+	DoorDirection(String direction) {
+		temp = direction;
+	}
+}
