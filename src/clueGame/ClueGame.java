486ccdebb6530f56e4dbea9f7011d1181e5e0603
Nathan Lambert <nlambert@mymail.mines.edu>
2019-04-10 00:26:58 -0600

    Added fixed CLueGame and readded ControlGUI. Added main window for game, just have to draw game board

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -3,92 +3,22 @@
 package clueGame;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
 
-public class ClueGame extends JPanel{
+public class ClueGame extends JFrame{
 	
-	public ClueGame() {		
-		setLayout(new GridLayout(2, 0));
-		JPanel gameBoardPanel = new JPanel();
-		gameBoardPanel.setLayout(new GridLayout(Board.getNumRows(), Board.getNumColumns()));
-		
-		JPanel topControlPanel = new JPanel();
-		topControlPanel.setLayout(new GridLayout(1, 0));
-		topControlPanel.add(createTurnPanel());
-		topControlPanel.add(createButtonPanel());
-		add(topControlPanel);
-		
-		JPanel bottomControlPanel = new JPanel();
-		bottomControlPanel.setLayout(new FlowLayout());
-		bottomControlPanel.add(createDieRollPanel());
-		bottomControlPanel.add(createGuessPanel());
-		bottomControlPanel.add(createGuessResultPanel());
-		add(bottomControlPanel);
-	}
+	private Board board;
+	JPanel panel;
+	private static JFrame frame;
 	
-	
-	public JPanel createTurnPanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(2, 1));
-		JLabel turnLabel = new JLabel("Whose turn?");
-		JTextField name = new JTextField(20);
-		name.setEditable(false);
-		panel.add(turnLabel);
-		panel.add(name);
-		panel.setBorder(new EtchedBorder());
-		return panel;
-	}
-	
-	public JPanel createButtonPanel() {
-		//default uses flow layout
-		JPanel panel = new JPanel();
-		JButton nextPlayer = new JButton("Next player");
-		JButton makeAccusation = new JButton("Make accusation");
-		panel.add(nextPlayer);
-		panel.add(makeAccusation);
-		return panel;
-	}
-	
-	public JPanel createDieRollPanel() {
-		JPanel panel = new JPanel();
-		JTextField dieRoll = new JTextField(5);
-		dieRoll.setEditable(false);
-		panel.add(dieRoll);
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Die Roll"));
-		return panel;
-	}
-	
-	public JPanel createGuessPanel() {
-		JPanel panel = new JPanel();
-		JTextField guess = new JTextField(30);
-		guess.setEditable(false);
-		panel.add(guess);
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
-		return panel;
-	}
-	
-	public JPanel createGuessResultPanel() {
-		JPanel panel = new JPanel();
-		JTextField response = new JTextField(18);
-		response.setEditable(false);
-		panel.add(response);
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Response to Guess"));
-		return panel;
-	}
-	
-	
-	public static void main(String[] args) {
+	public ClueGame() {
 		// Board is singleton, get the only instance
 		Board board;
 		board = Board.getInstance();
@@ -96,21 +26,47 @@ public class ClueGame extends JPanel{
 		board.setConfigFiles("data/BoardLayout.csv", "data/ClueRooms.txt", "data/CluePlayers.txt", "data/ClueWeapons.txt");		
 		// Initialize will load BOTH config files 
 		board.initialize();
-		//creates the main JFrame
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(700, 750);
-		ClueGame control = new ClueGame();
-		frame.add(control, BorderLayout.CENTER);
 		
-		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-		}
-		catch(Exception e) {
-			System.err.println("Look and feel not set");
-		}
+		panel = board;
+		add(panel, BorderLayout.CENTER);
 		
-		//view the JFrame
-		frame.setVisible(true);
+		JPanel controlGui = new ControlGUI();
+		add(controlGui, BorderLayout.SOUTH);
+		
+		//add the menu bar
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(createFileMenu());
 	}
+	
+	private JMenu createFileMenu() {
+		JMenu menu = new JMenu("File");
+		menu.add(createFileExitItem());
+		return menu;
+	}
+	
+	private JMenuItem createFileExitItem() {
+		JMenuItem item = new JMenuItem("Exit");
+		class MenuItemListener implements ActionListener{
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		}
+		item.addActionListener(new MenuItemListener());
+		return item;
+	}
+	
+	public static void main(String[] args) {
+		ClueGame game = new ClueGame();
+		frame = game;
+		frame.setSize(700, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Clue Game");
+		
+		game.setVisible(true);
+		
+		
+	}
+	
+	
 }
---
+++ src/clueGame/ControlGUI.java
@@ -0,0 +1,112 @@
+//Authors: Nathan Lambert, Elliott McCabe
+
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class ControlGUI extends JPanel{
+	
+	public ControlGUI() {		
+		setLayout(new GridLayout(2, 0));
+		
+		JPanel topControlPanel = new JPanel();
+		topControlPanel.setLayout(new GridLayout(1, 0));
+		topControlPanel.add(createTurnPanel());
+		topControlPanel.add(createButtonPanel());
+		add(topControlPanel);
+		
+		JPanel bottomControlPanel = new JPanel();
+		bottomControlPanel.setLayout(new FlowLayout());
+		bottomControlPanel.add(createDieRollPanel());
+		bottomControlPanel.add(createGuessPanel());
+		bottomControlPanel.add(createGuessResultPanel());
+		add(bottomControlPanel);
+	}
+
+
+	public JPanel createTurnPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 1));
+		JLabel turnLabel = new JLabel("Whose turn?");
+		JTextField name = new JTextField(20);
+		name.setEditable(false);
+		panel.add(turnLabel);
+		panel.add(name);
+		panel.setBorder(new EtchedBorder());
+		return panel;
+	}
+	
+	public JPanel createButtonPanel() {
+		//default uses flow layout
+		JPanel panel = new JPanel();
+		JButton nextPlayer = new JButton("Next player");
+		JButton makeAccusation = new JButton("Make accusation");
+		panel.add(nextPlayer);
+		panel.add(makeAccusation);
+		return panel;
+	}
+	
+	public JPanel createDieRollPanel() {
+		JPanel panel = new JPanel();
+		JTextField dieRoll = new JTextField(5);
+		dieRoll.setEditable(false);
+		panel.add(dieRoll);
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Die Roll"));
+		return panel;
+	}
+	
+	public JPanel createGuessPanel() {
+		JPanel panel = new JPanel();
+		JTextField guess = new JTextField(30);
+		guess.setEditable(false);
+		panel.add(guess);
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
+		return panel;
+	}
+	
+	public JPanel createGuessResultPanel() {
+		JPanel panel = new JPanel();
+		JTextField response = new JTextField(18);
+		response.setEditable(false);
+		panel.add(response);
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Response to Guess"));
+		return panel;
+	}
+	
+	
+	//main function for testing
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(700, 150);
+		ControlGUI control = new ControlGUI();
+		frame.add(control, BorderLayout.CENTER);
+		
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+		}
+		catch(Exception e) {
+			System.err.println("Look and feel not set");
+		}
+		
+		//view the JFrame
+		frame.setVisible(true);
+	}
+}
