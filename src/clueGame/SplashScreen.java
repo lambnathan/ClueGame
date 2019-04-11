260a9db4a92b1e09cc1488d490585bb8ba576b1b
Nathan Lambert <nlambert@mines.edu>
2019-04-11 14:04:01 -0600

    Added title for splash screen

--- src/clueGame/SplashScreen.java
+++ src/clueGame/SplashScreen.java
@@ -7,9 +7,8 @@ public class SplashScreen extends JOptionPane {
 	public SplashScreen() {
 		setName("Test");
 		setSize(600,250);
-		
 		String message = "You are Miss Scarlet, use the 'next player' button to take your turn.";
-		JOptionPane.showMessageDialog(null, message);
+		JOptionPane.showMessageDialog(null, message, "Welcome to Clue", JOptionPane.INFORMATION_MESSAGE);
 	}
 	
 }
